4ade8c3c268534c1c28461589333ad24ede7d684
Herman <nitrowitty@gmail.com>
2023-10-06 22:47:22 -0600

    home

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+import {
+	RiGithubLine,
+	RiInstagramLine,
+	RiLinkedinLine,
+	RiTwitterLine,
+	RiYoutubeLine
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+		<div className="flex items-center gap-x-5 text-lg">
+			<Link href={''} className="hover:text-accent transition-all duration-300">
+				<RiYoutubeLine />
+			</Link>
+			<Link href={''} className="hover:text-accent transition-all duration-300">
+				<RiInstagramLine />
+			</Link>
+			<Link href={''} className="hover:text-accent transition-all duration-300">
+				<RiLinkedinLine />
+			</Link>
+			<Link href={''} className="hover:text-accent transition-all duration-300">
+				<RiTwitterLine />
+			</Link>
+			<Link href={''} className="hover:text-accent transition-all duration-300">
+				<RiGithubLine />
+			</Link>
+		</div>
+	);
 };
 
 export default Socials;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import Layout from '@/components/Layout'
+import Transition from '@/components/Transition'
 import '@/styles/globals.css'
+import { AnimatePresence, motion } from 'framer-motion'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+	const router = useRouter()
   return <Layout>
-		<Component {...pageProps} />
+		<AnimatePresence mode="wait">
+			<motion.div key={router.route} className='h-full'>
+				<Transition />
+				<Component {...pageProps} />
+			</motion.div>
+			
+		</AnimatePresence>
 	</Layout>
 }
