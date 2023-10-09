0c76dea05e1b21422c057809911650f61a096627
Herman <nitrowitty@gmail.com>
2023-10-09 09:00:24 -0600

    testimonial

--- src/components/TestimonialSlider.tsx
+++ src/components/TestimonialSlider.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import { FaQuoteLeft } from "react-icons/fa";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 // testimonial data
 const testimonialData = [
   {
@@ -24,7 +33,41 @@ const testimonialData = [
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+		<Swiper 
+			navigation={true}
+			pagination={{clickable: true}}
+			modules={[Navigation, Pagination]}
+			className='h-[400px]'
+		>
+			{testimonialData.map((item, index) => (
+				<SwiperSlide key={index}>
+					<div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16 ">
+						<div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:m-0">
+							<div className="flex flex-col justify-center text-center">
+								<div className="mb-2 mx-auto">
+									<Image src={item.image} alt={item.name} width={100} height={100} />
+								</div>
+								<div className="text-lg">
+									{item.name}
+								</div>
+								<div className="text-[12px] uppercase font-extralight tracking-widest">
+									{item.position}
+								</div>
+							</div>
+						</div>
+						<div className="flex-1 flex flex-col justify-center before:w-[1px] 
+						xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+							<div className="mb-4">
+								<FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0"/>
+							</div>
+							<div className="xl:text-lg text-center md:text-left">{item.message}</div>
+						</div>
+					</div>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 };
 
 export default TestimonialSlider;
--- src/pages/testimonials/index.tsx
+++ src/pages/testimonials/index.tsx
@@ -1,5 +1,29 @@
+import TestimonialSlider from '@/components/TestimonialSlider';
+import { fadeIn } from '@/variants';
+import { motion } from 'framer-motion';
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+		<div className="h-full bg-primary/30 py-32 text-center">
+			<div className="container mx-auto h-full flex flex-col justify-center">
+				<motion.h2
+					variants={fadeIn('up', 0.2)}
+					initial='hidden'
+					animate='show'
+					exit='hidden'  
+					className="h2 mb-8 xl:mb-0">
+					What people <span className="text-accent">say.</span>
+				</motion.h2>
+				<motion.div
+					variants={fadeIn('up', 0.4)}
+					initial='hidden'
+					animate='show'
+					exit='hidden' >
+					<TestimonialSlider />
+				</motion.div>
+			</div>
+		</div>
+	);
 };
 
 export default Testimonials;
