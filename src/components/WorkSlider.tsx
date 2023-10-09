ba42dbf842f8e1e6b518a1caf67858a04d41d568
Herman <nitrowitty@gmail.com>
2023-10-09 03:46:23 -0600

    work

--- src/components/WorkSlider.tsx
+++ src/components/WorkSlider.tsx
@@ -1,4 +1,16 @@
-// data
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { Pagination } from 'swiper/modules'
+
+import Image from 'next/image'
+
+import { BsArrowRight } from 'react-icons/bs'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
 const workSlides = {
   slides: [
     {
@@ -44,8 +56,44 @@ const workSlides = {
   ],
 };
 
+
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+		<Swiper 
+			spaceBetween={10}
+			pagination={{clickable: true}}
+			modules={[Pagination]}
+			className='h-[280px] sm:h-[480px]'
+		>
+			{workSlides.slides.map((item, index) => (
+				<SwiperSlide key={index}>
+					<div className='grid grid-cols-2 grid-rows-2 gap-4'>
+						{item.images.map((images, index) => (
+							<div key={index} className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+								<div className='flex items-center justify-center relative overflow-hidden group'>
+									<Image src={images.path} alt={images.title} width={500} height={300} />
+									<div className='absolute inset-0 cursor-pointer bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd]
+									opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+									<div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10
+									group-hover:xl:-translate-y-20 transition-all duration-300'>
+										<div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+											<div className='delay-100'>LIVE</div>
+											<div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+												PROJECT
+											</div>
+											<div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+												<BsArrowRight />
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 };
 
 export default WorkSlider;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -190,35 +190,35 @@ const About = () => {
 					animate="show"
 					exit="hidden" 
 					className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
-						<div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-							{aboutData.map((item, key) => (
-								<div 
-									key={key} 
-									className={
-										`${index === key && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} 
-										cursor-pointer capitalize xl:text-lg relative after:w-8 
-										after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-									onClick={() => setIndex(key)}>
-									{item.title}
-								</div>
-							))}
-						</div>
-						<div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-							{aboutData[index].info.map((item, itemIndex) => (
-								<div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
-									<div className="font-light mb-2 md:mb-0">{item.title}</div>
-									<div className="hidden md:flex">-</div>
-									<div>{item.stage}</div>
-									<div className="flex gap-x-4">
-										{item.icons.map((icon, itemIndex) => (
-										<div key={itemIndex} className="text-2xl text-white">
-											{icon}
-										</div>
-									))}
+					<div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+						{aboutData.map((item, key) => (
+							<div 
+								key={key} 
+								className={
+									`${index === key && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} 
+									cursor-pointer capitalize xl:text-lg relative after:w-8 
+									after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+								onClick={() => setIndex(key)}>
+								{item.title}
+							</div>
+						))}
+					</div>
+					<div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+						{aboutData[index].info.map((item, itemIndex) => (
+							<div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+								<div className="font-light mb-2 md:mb-0">{item.title}</div>
+								<div className="hidden md:flex">-</div>
+								<div>{item.stage}</div>
+								<div className="flex gap-x-4">
+									{item.icons.map((icon, itemIndex) => (
+									<div key={itemIndex} className="text-2xl text-white">
+										{icon}
 									</div>
+								))}
 								</div>
-							))}
-						</div>
+							</div>
+						))}
+					</div>
 				</motion.div>
 			</div>
 		</div>
