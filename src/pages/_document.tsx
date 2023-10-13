7bb1cf763f28cf60e4ffea4936609d6db90048a2
Herman <nitrowitty@gmail.com>
2023-10-13 10:02:16 -0600

    fix meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-				<meta property="twitter:image" content="Twitter link preview image URL" />
-				<meta property="twitter:card" content="summary_large_image"></meta>
+				<meta property="twitter:image" content="/previewpage.jpeg" />
+				<meta property="twitter:card" content="/previewpage.jpeg"></meta>
 				<meta property="twitter:title" content="Ronindev Elixir developer Portfolio"></meta>
 				<meta property="twitter:description" content="Frontend developer, passionate about Elixir and Phoenix portfolio"></meta>
 				<meta property="description" content="Frontend developer, passionate about Elixir and Phoenix portfolio" />
-				<meta property="og:image" content="Link preview image URL"></meta>
+				<meta property="og:image" content="/previewpage.jpeg"></meta>
 				<meta property="og:title" content="Ronindev Elixir developer Portfolio"></meta>
 				<meta property="og:description" content="Frontend developer, passionate about Elixir and Phoenix portfolio" />
 				<meta property="og:url" content="https://ronindev.ninja"></meta>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ import Head from "next/head";
 export default function Home() {
   return (
 		<>
-				<Head>
-					<title>Ronindev Home - Elixir developer</title>
-				</Head>
+			<Head>
+				<title>Ronindev Home - Elixir developer</title>
+			</Head>
 			<div className="bg-primary/60 h-full">
 				<div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
 					<div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
