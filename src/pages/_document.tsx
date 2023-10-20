42ee4d3eb01dfad7495f3951b7a8e98884033402
Herman <nitrowitty@gmail.com>
2023-10-20 08:48:06 -0600

    google analitics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
 
 export default function Document() {
   return (
@@ -15,7 +17,18 @@ export default function Document() {
 				<meta property="og:description" content="Frontend developer, passionate about Elixir and Phoenix portfolio" />
 				<meta property="og:url" content="https://ronindev.ninja" />
 			</Head>
+			
       <body>
+				<Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-5V62HRZ133');
+        `}
+      </Script>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-5V62HRZ133" />
         <Main />
         <NextScript />
       </body>
